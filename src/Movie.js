99d758ffee62a655bccf08cb41bc9c4b2fecbea8
parkkyungdong <gdpark0102@daum.net>
2020-03-04 15:46:58 +0900

    test

--- src/Movie.js
+++ src/Movie.js
@@ -17,7 +17,6 @@ class Movie extends Component{
         }
 
     }
-
     getMo = () =>{
         const {data : {data : {movies}}} = axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
 
